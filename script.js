03bfc07000991ff03f52fded702bf14ec56e1524
CopperCoder227 <526csteiner@frhsd.com>
2024-11-26 08:35:03 -0500

    tings i have from old thing

---
+++ script.js
@@ -0,0 +1,40 @@
+let userName = 'Cooper';
+const userQuestion = 'Lunch?';
+let randomNumber = Math.floor(Math.random() * 8);
+let eightBall = '';
+
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+
+console.log(`${userName} asked: ${userQuestion}`);
+if (randomNumber >= 0) {
+  switch (randomNumber) {
+  case 0:
+    eightBall = 'It is certain';
+    break;
+  case 1:
+    eightBall = 'It is decidedly so';
+    break;
+  case 2:
+    eightBall = 'Reply hazy try again';
+    break;
+  case 3:
+    eightBall = 'Cannot predict now';
+    break;
+  case 4:
+    eightBall = 'Do not count on it';
+    break;
+  case 5:
+    eightBall = 'My sources say no';
+    break;
+  case 6:
+    eightBall = 'Outlook not so good';
+    break;
+  case 7:
+    eightBall = 'Signs point to yes';
+    break; 
+  }
+} else {
+  console.log(`HOW DARE YOU BREAK THE BALL WITH A NUMBER 8!!! GET OFT THIS WEBSITE AND BE GONE FROM THIS PLANE OF EXISTANCE!!!`);
+}
+console.log(`Magic Eightball: ${eightBall}`);
