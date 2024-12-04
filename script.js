432c41b5df5f13131a776d02b9cc67f36a90f2ce
CopperCoder227 <526csteiner@frhsd.com>
2024-12-04 09:20:45 -0500

    FINAL COMMIT

--- script.js
+++ script.js
@@ -10,27 +10,27 @@ const userQuestion = prompt("WHAT IS YOUR QUESTION FOR ELMO?????");
 if (randomNumber >= 0) {
   switch (randomNumber) {
   case 0:
-    eightBall = 'Elmo says its certain';
+    eightBall = 'Elmo says its certain.';
     document.getElementById("response-img").src = "imgs/certant.gif"
     document.getElementById("response-img").classList.remove("dont")
     break;
   case 1:
-    eightBall = 'Elmo says its decidedly so';
+    eightBall = 'Elmo says its decidedly so.';
     document.getElementById("response-img").src = "imgs/decide.gif"
     document.getElementById("response-img").classList.remove("dont")
     break;
   case 2:
-    eightBall = 'Elmo couldnt understand the question. Elmo says try again';
+    eightBall = 'Elmo couldnt understand the question. Elmo says try again.';
     document.getElementById("response-img").src = "imgs/try.gif"
     document.getElementById("response-img").classList.remove("dont")
     break;
   case 3:
-    eightBall = 'Elmo is tired. Elmo can not predict your question now';
+    eightBall = 'Elmo is tired. Elmo can not predict your question now.';
     document.getElementById("response-img").src = "imgs/cant.gif"
     document.getElementById("response-img").classList.remove("dont")
     break;
   case 4:
-    eightBall = 'Elmo says that you shouldnt  count on it';
+    eightBall = 'Elmo says that you shouldnt  count on it.';
     document.getElementById("response-img").src = "imgs/dont.gif" 
     document.getElementById("response-img").classList.add("dont")
     break;
@@ -40,7 +40,7 @@ if (randomNumber >= 0) {
     document.getElementById("response-img").classList.remove("dont")
     break;
   case 6:
-    eightBall = 'Elmo dosent think your outlook is so good';
+    eightBall = 'Elmo dosent think your outlook is so good.';
     document.getElementById("response-img").src = "imgs/otlook.gif"
     document.getElementById("response-img").classList.remove("dont")
     break;
